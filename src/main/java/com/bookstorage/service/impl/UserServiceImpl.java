c9d798f710c9ea1ab2c5e3334773ecf0d3bd3f66
IgorArsionov <valisgvalis@gmail.com>
2026-04-19 22:05:38 +0300

    Added transactional annotation

--- src/main/java/com/bookstorage/service/impl/UserServiceImpl.java
+++ src/main/java/com/bookstorage/service/impl/UserServiceImpl.java
@@ -14,8 +14,10 @@ import com.bookstorage.service.UserService;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
